f094e4bf5f21a2473ceafad3268b60b7050b71be
vitalikM91 <maldur.vitalik@gmail.com>
2013-08-27 16:31:55 +0300

    Gps tracking

--- BikeNavigation/src/com/android/maps/MapActivity.java
+++ BikeNavigation/src/com/android/maps/MapActivity.java
@@ -1,12 +1,21 @@
 package com.android.maps;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -16,32 +25,30 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 
-public class MapActivity extends Activity {
-
+public class MapActivity extends Activity implements OnMapClickListener {
+	// Global constants
+    /*
+     * Define a request code to send to Google Play services
+     * This code is returned in Activity.onActivityResult
+     */
+    private final static int
+            CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+   
+	private GoogleMap map;
+	private GPSTracker locationManager;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		LatLng HAMBURG = new LatLng(53.558, 9.927);
-		LatLng KIEL = new LatLng(53.551, 9.993);
-		GoogleMap map;
-		
+	protected void onCreate(Bundle savedInstanceState) {	
 		super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_map);
-	    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-	        .getMap();
-	    Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-	        .title("Hamburg"));
-	    Marker kiel = map.addMarker(new MarkerOptions()
-	        .position(KIEL)
-	        .title("Kiel")
-	        .snippet("Kiel is cool")
-	        .icon(BitmapDescriptorFactory
-	            .fromResource(R.drawable.ic_launcher)));
-
-	    // Move the camera instantly to hamburg with a zoom of 15.
-	    map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
-
-	    // Zoom in, animating the camera.
-	    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+	    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+	    map.setOnMapClickListener(this);
+	    map.setMyLocationEnabled(true);
+	    locationManager = new GPSTracker(this);
+	    Location currentLocation = locationManager.getLocation();
+	    LatLng loc = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()); 
+	    map.moveCamera(CameraUpdateFactory.newLatLng(loc));
+	  	map.animateCamera(CameraUpdateFactory.zoomTo(10));
 	}
 
 	@Override
@@ -50,8 +57,13 @@ public class MapActivity extends Activity {
 		getMenuInflater().inflate(R.menu.map, menu);
 		return true;
 	}
+
+	@Override
+	public void onMapClick(LatLng point) {
+		// TODO Auto-generated method stub
+		map.addMarker(new MarkerOptions().position(point).title("Point"));
+	}
 	
-	// ============================================================================
 	public void returnToStart (View view) {
 	
 		Intent intent = new Intent(this, FullscreenActivity.class);
